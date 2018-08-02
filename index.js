8ed4a475d2c193cae59f788d6701043166040efd
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-02 13:30:43 -0700

    JSON Loading Functionality!
    - now loads JSON files when specified
    - currently only loads "contact.json" as that's the only filled one
    may need to have different load methods for different types of data... not all are in same format!

--- index.js
+++ index.js
@@ -2,7 +2,38 @@
 !function() {
 	
 	window.addEventListener("load", function() {
-		
+		loadInfo("contact");
 	});
+	
+	function loadInfo(name) {
+		let section = $(name);
+		let load = function(json) { 
+			let data = JSON.parse(json);
+			for (let i = 0; i < data.length; i++) {
+				let datum = data[i];
+				
+				// loading title
+				let title = ce("h3");
+				title.textContent = datum.title;
+				// loading text (link or paragraph)
+				let text = undefined;
+				if (datum.link === undefined) {
+					text = ce("p");
+				} else {
+					text = ce("a");
+					text.href = datum.link;
+					text.target = "_blank"; // to open in new tab
+				}
+				text.textContent = datum.text;
+				
+				// putting everything into the HTML
+				let div = ce("div");
+				div.appendChild(title);
+				div.appendChild(text);
+				section.appendChild(div);
+			}
+		}
+		ajaxGET("info/" + name + ".json", load);
+	}
 	
 }();
